efc3ff9515ac47d6dc27d8c06013c0b6036cd2ec
adam baxter & Doug James <pair+adamscottbaxter.douglaspjames@devbootcamp.com>
2016-08-04 14:11:49 -0500

    collect drafters from the form, select users view

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,9 +1,29 @@
 class SchedulesController < ApplicationController
+skip_before_action :verify_authenticity_token, :only => [:new]
 
   def show
     @schedule = Schedule.last
     render 'show'
   end
+
+  def select_users
+    @users ||= User.all
+    @draftlist = @users
+    render 'drafts/select_users'
+  end
+
+  def new
+    p params.inspect
+    @users = []
+    params[:users].each do |value|
+      p "---"
+      p "#{value}"
+      @users << User.find(value.to_i)
+    end
+    p "-------------users----------"
+      p @users
+  end
+
 
 
 end
