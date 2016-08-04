8283f6f8995e600ebdbd8c6d1d821a8f989aa5d1
adam baxter & Doug James <pair+adamscottbaxter.douglaspjames@devbootcamp.com>
2016-08-04 15:13:39 -0500

    add date form to schedule generation

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -13,11 +13,17 @@ skip_before_action :verify_authenticity_token, :only => [:new]
   end
 
   def new
+    @schedule = Schedule.create(start_date: params[:start_date])
     @users = []
     params[:users].each do |value|
       @users << User.find(value.to_i)
     end
+    @users.shuffle!
+    @users2 = @users + @users.reverse
+    render 'schedules/new'
   end
+
+  # def post
 
 
 
