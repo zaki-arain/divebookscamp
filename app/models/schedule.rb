3638df266b97cca7e3df98296c867640ae68fb86
Dumitru Gutu & Max Greschl <pair+dumitrugutu.mgreschl@devbootcamp.com>
2016-08-04 14:59:18 -0500

    add associations to selection, add find_selection method to schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,7 +1,11 @@
 class Schedule < ApplicationRecord
 
-has_many :selections
-has_many :users, through: :selections
-has_many :tasks, through: :selections
+  has_many :selections
+  has_many :users, through: :selections
+  has_many :tasks, through: :selections
+
+  def find_selection(args)
+    self.selections.select { |selection| selection.day.id == args[:day_id] && selection.group.id == args[:group_id]}
+  end
 
 end
--- app/models/selection.rb
+++ app/models/selection.rb
@@ -2,5 +2,6 @@ class Selection < ApplicationRecord
 	belongs_to :schedule
 	belongs_to :user
 	belongs_to :task
-	# has_one :group, through: :task
+	has_one :day, through: :task
+  has_one :group, through: :task
 end
