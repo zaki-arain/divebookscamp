f0869acba1ab1f95fe50995a7353b916b3d4bb98
adam baxter & Doug James <pair+adamscottbaxter.douglaspjames@devbootcamp.com>
2016-08-05 14:06:08 -0500

    add logic for advancing draft order during draft

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -20,9 +20,8 @@ skip_before_action :verify_authenticity_token, :only => [:new]
     end
     @users.shuffle!
     @snaked_users = @users + @users.reverse
-    @drafters = []
-    @snaked_users.each do |user|
-      @drafters << Drafter.create(user_id: user.id, schedule_id: @schedule.id)
+    @drafters = @snaked_users.map do |user|
+      Drafter.create(user_id: user.id, schedule_id: @schedule.id)
     end
 
 
@@ -32,14 +31,18 @@ skip_before_action :verify_authenticity_token, :only => [:new]
   def edit
     @schedule = Schedule.find(params[:schedule_id])
     @selection = Selection.create!(user_id: session[:user_id], schedule_id: @schedule.id, task_id: params[:id_to_update])
-    @schedule = Schedule.find(params[:schedule_id])
-    current_drafter = @schedule.drafters[0]
+    current_drafter = @schedule.drafters.order(:id).first
 
-    p "----- in edit"
     if request.xhr?
-      Drafter.create(user_id: current_drafter.user.id, schedule_id: @schedule.id)
+      Drafter.create(user_id: session[:user_id], schedule_id: @schedule.id)
       current_drafter.destroy
-      render 'schedules/_no_links_show', layout: false, locals: {schedule: @schedule}
+      @schedule = Schedule.find(params[:schedule_id])
+      current_drafter = @schedule.drafters.order(:id).first
+      if current_drafter.user_id == current_user.id
+        render 'schedules/_links_show', layout: false, locals: {schedule: @schedule}
+      else
+        render 'schedules/_no_links_show', layout: false, locals: {schedule: @schedule}
+      end
     else
       "booo"
     end
@@ -47,18 +50,12 @@ skip_before_action :verify_authenticity_token, :only => [:new]
 
   def draft
     @schedule = Schedule.find(params[:schedule_id])
-    current_drafter = @schedule.drafters[0]
+    current_drafter = @schedule.drafters.order(:id).first
     p "------ in draft route---"
     p current_drafter
     if current_drafter.user_id == current_user.id
-      p "current drafter -=-=-=-=-=0-=0-=0=-0=-=--=0=0"
-      Drafter.create(user_id: current_drafter.user.id, schedule_id: @schedule.id)
-      current_drafter.destroy
       render 'schedules/edit'
-
     else
-      p "NONOTNOTNOTNOTTNOcurrent drafter -=-=-=-=-=0-=0-=0=-0=-=--=0=0"
-
       render 'schedules/edit_no_links'
     end
   end
